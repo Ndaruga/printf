9faa30a9b5056bd522a49906cd4c8c54a59dc726
Ndarugaa <ndaruga.francis@gamil.com>
2023-04-24 20:54:25 +0300

    Initialize print f

---
+++ printf.c
@@ -0,0 +1,9 @@
+#include "main.h"
+#include "stdlib.h"
+
+int _printf(const char *format, ...)
+{
+    char format_types[5] = {'d','f', 'x'};
+    
+    char special_chars[5] = {'\\', '%', '\"', }
+}
