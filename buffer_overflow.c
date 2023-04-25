400ba8aff0c154931c665be46a8467313fb03f88
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-26 00:00:33 +0300

    buffer overflow

---
+++ buffer_overflow.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * check_buffer_overflow - if writing over buffer space,
+ * print everything then revert length back to 0 to write at buffer start
+ * @buffer: buffer holding string to print
+ * @len: position in buffer
+ * Return: length position
+ */
+int check_buffer_overflow(char *buffer, int len)
+{
+	if (len > 1020)
+	{
+		write(1, buffer, len);
+		len = 0;
+	}
+	return (len);
+}
--- printf.c
+++ printf.c
@@ -1,9 +1,95 @@
 #include "main.h"
 #include "stdlib.h"
+#include "stdarg.h"
+
 
 int _printf(const char *format, ...)
 {
-    char format_types[5] = {'d','f', 'x'};
-    
-    char special_chars[5] = {'\\', '%', '\"', }
+	int len = 0, i = 0, j = 0, total_len = 0;
+	va_list list;
+	char* (*f)(va_list);
+	char *str, *buffer;
+
+	if (format == NULL)
+		return (-1);
+
+	buffer = create_buffer();
+	if (buffer == NULL)
+		return (-1);
+
+	va_start(list, format);
+
+	while (format[i] != '\0') /* check if text format is null terminator*/
+	{
+		if (format[i] != '%') /* copy text format into buffer until '%' */
+		{
+			len = check_buffer_overflow(buffer, len);
+			buffer[len++] = format[i++];
+			total_len++;
+		}
+		else /* Handle the % sign by checling its next format specifier*/
+		{
+			i++;
+			if (format[i] == '\0') /* handle single ending % */
+			{
+				va_end(list);
+				free(buffer);
+				return (-1);
+			}
+			if (format[i] == '%') /* handle double %'s */
+			{
+				len = check_buffer_overflow(buffer, len);
+				buffer[len++] = format[i];
+				total_len++;
+			}
+			else
+			{
+				f = get_func(format[i]); /* grab function */
+				if (f == NULL)  /* handle fake id */
+				{
+					len = check_buffer_overflow(buffer, len);
+					buffer[len++] = '%'; total_len++;
+					buffer[len++] = format[i]; total_len++;
+				}
+				else /* return string, copy to buffer */
+				{
+					str = f(list);
+					if (str == NULL)
+					{
+						va_end(list);
+						free(buffer);
+						return (-1);
+					}
+					if (format[i] == 'c' && str[0] == '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = '\0';
+						total_len++;
+					}
+					j = 0;
+					while (str[j] != '\0')
+					{
+						len = check_buffer_overflow(buffer, len);
+						buffer[len++] = str[j];
+						total_len++; j++;
+					}
+					free(str);
+				}
+			} i++;
+		}
+	}
+	write_buffer(buffer, len, list);
+	return (total_len);
+}
+
+/**
+ * main - sample main program
+ * Return: 0 on sucess
+ */
+int main(void)
+{
+	_printf("\n\n\nHere's some examples of what you could do with this custom_printf function!\n\n\n");
+	sleep(1);
+	_printf("\nPrinting Strings, Characters, and Numbers...... %s %c%drld\n\n", "Hello", 'W', 0);
+	return (0);
 }
