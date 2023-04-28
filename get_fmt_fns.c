fec8b2f6fd683265b08913c9eb09b6a53fbce44d
Ndarugaa <ndaruga.francis@gmail.com>
2023-04-28 23:45:45 +0300

    modify format functions

--- get_fmt_fns.c
+++ get_fmt_fns.c
@@ -4,9 +4,9 @@
 
 
 /**
- * get_func - returns needed function
- * @i: identifier for function
- * Return: Pointer to needed function
+ * get_func - returns required function
+ * @i: identifier for functional format
+ * Return: Pointer to required function
  */
 char* (*get_func(char i))(va_list)
 {
@@ -15,6 +15,12 @@ char* (*get_func(char i))(va_list)
 	print_fn keys[] = {
 		{'c', print_c},
 		{'s', print_s},
+		{'d', print_d},
+		{'i', print_d},
+		{'b', itob},
+		{'R', rot13},
+		{'r', rev_string},
+		{'o', itoOctal},
 		{'\0', NULL}
 	};
 
